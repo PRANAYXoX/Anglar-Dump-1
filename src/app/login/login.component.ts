41ab40606a51f4ee70aae0991b4d5c5163806f97
pranayXoX <pranay.dmnsns@gmail.com>
2022-07-31 03:37:22 +0530

    Created a counter

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  disVal = 0;
+  valAdd(val: string) {
+    this.disVal = parseInt(val) + 1;
   }
-
+  valDec(val: string) {
+    parseInt(val) == 0
+      ? alert('Invald Operation!')
+      : (this.disVal = parseInt(val) - 1);
+  }
 }
