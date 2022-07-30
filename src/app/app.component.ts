42cb5813a002d2a8a4461bed6e11718a095f338c
pranayXoX <pranay.dmnsns@gmail.com>
2022-07-30 23:30:24 +0530

    Components Study

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'application_1';
+  num1 = 500;
+  num2 = 600;
+  getVal() {
+    return 'SUM ID:' + (this.num1 + this.num2);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
